40b4fc3d4896d3149e4d40e5d5d220cc726c7f6d
Kamil <kamilcz2001@gmail.com>
2025-02-26 11:39:17 +0100

    feat: Add registration function in userController and /register route in userRoutes

--- src/controllers/customerController.ts
+++ src/controllers/customerController.ts
@@ -1,8 +1,9 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { prisma } from "../lib/prisma";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { TypedRequestBody } from "../types/customer.types";
+import { hashPassword } from "../utils/hash";
 
 const secretKey = process.env.SECRET_KEY as string;
 
@@ -45,6 +46,67 @@ export const login = async (
     res.json({ token });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+    return;
+  }
+};
+
+export const register = async (
+  req: TypedRequestBody<{
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+  }>,
+  res: Response
+): Promise<void> => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    if (!firstName || !lastName || !email || !password) {
+      res.status(400).json({
+        message: "First name, last name, email, and password are required",
+      });
+      return;
+    }
+
+    const existingCustomer = await prisma.customer.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (existingCustomer) {
+      res.status(400).json({ message: "Email already exist" });
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const customer = await prisma.customer.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    const token = jwt.sign(
+      {
+        sub: customer.id,
+        role: customer.role,
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor(Date.now() / 1000) + 60 * 60,
+      },
+      secretKey,
+      { algorithm: "HS256" }
+    );
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+
     res.status(500).json({ message: "Internal Server Error" });
     return;
   }
--- src/routes/customerRoutes.ts
+++ src/routes/customerRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { login } from "./../controllers/customerController";
+import { login, register } from "./../controllers/customerController";
 
 export const customerRoutes: Router = Router();
 
 customerRoutes.post("/login", login);
+customerRoutes.post("/register", register);
